208e41a60346fa8fdc731180c954c97743fb02d9
Giri Yaram <yaramgiri@gmail.com>
2024-10-08 20:03:23 +0530

    added homepage components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import Chat from "@/components/Chat";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import { PipelineUI } from "@/ui";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   return (
-   <div>
-    React Flow
-   </div>
+    <div>
+      <Navbar />
+      <Sidebar />
+      <ToastContainer
+        autoClose="3000"
+        pauseOnHover={false}
+        hideProgressBar={false}
+        className="toast-container-custom"
+      />
+      <Chat />
+      <PipelineUI />
+    </div>
   );
 }
