f6cad7de350d52a732d1f196fc93f0e13cad1254
Giri Yaram <yaramgiri@gmail.com>
2024-10-08 20:00:47 +0530

    added chat page

---
+++ src/app/chat/page.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import ChatContainer from '@/components/chats/ChatContainer';
+import ChatSidebar from '@/components/chats/ChatSidebar';
+import { useChatStore } from '@/store';
+import React from 'react';
+
+
+const ChatPage = () => {
+  const { chats, activeChatId, createNewChat, sendMessage } = useChatStore();
+
+  return (
+    <div className="chat-page flex">
+      <ChatSidebar 
+        chats={chats} 
+        activeChatId={activeChatId} 
+        createNewChat={createNewChat}
+      />
+      <ChatContainer 
+        chats={chats} 
+        activeChatId={activeChatId} 
+        onSendMessage={sendMessage} 
+      />
+    </div>
+  );
+};
+
+export default ChatPage;
