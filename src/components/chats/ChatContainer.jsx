269f9bd83707359e042c36d5743b88cfc8138463
Giri Yaram <yaramgiri@gmail.com>
2024-10-08 22:17:46 +0530

    added chat interface

--- src/components/chats/ChatContainer.jsx
+++ src/components/chats/ChatContainer.jsx
@@ -17,20 +17,43 @@ const ChatContainer = ({ chats, activeChatId, onSendMessage }) => {
 
   return (
     <div className="flex flex-col  flex-1 p-4 h-screen bg-[#FAFAFB]  relative">
-      <div className="flex items-center justify-center gap-2  h-16 w-full fixed border-b border-[#E3E6EA] bg-white z-20 -mr-4">
+      <div className="flex items-center justify-center gap-2  h-16 w-full fixed border-b rounded-t-xl border-[#E3E6EA] bg-white z-20 -mr-4">
         <Image src="/gala_add.png" alt="" width={35} height={35} />
         <h2 className="font-medium text-xl">AI Assistant</h2>
       </div>
       <div className="bg-white p-4 !pt-24 rounded-lg h-full overflow-y-auto">
         <div className="chat-messages">
           {activeChat?.messages.map((message, index) => (
-            <div
-              key={index}
-              className={`message ${
-                message.sender === "user" ? "user-message" : "bot-message"
-              }`}
-            >
-              {message.text}
+            <div key={index} className={`flex items-start mb-4 `}>
+              <div
+                className={`p-3 rounded-lg max-w-3xl text-base text-[#444] flex gap-3 ${
+                  message.sender === "user" ? "bg-white" : "bg-[#F7F8FA]"
+                }`}
+              >
+                {/* Profile Image */}
+              {message.sender !== "user" && (
+                <div className="mr-2">
+                  <Image
+                    src="/gala_add.png"
+                    alt="Bot Profile"
+                    width={30}
+                    height={30}
+                  />
+                </div>
+              )}
+              {/* Profile Image for User */}
+              {message.sender === "user" && (
+                <div className="mr-2">
+                  <Image
+                    src="/s.svg"
+                    alt="User Profile"
+                    width={30}
+                    height={30}
+                  />
+                </div>
+              )}
+                {message.text}
+              </div>
             </div>
           ))}
         </div>
